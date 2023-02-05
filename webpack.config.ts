342bd42f03f991c40c93ab7d2076f1f0e258eeab
Samat Toichuev <samat25774@gmail.com>
2023-02-05 21:17:57 +0600

    chagned git ignore

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,8 +3,6 @@ import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnv, BuildPath} from "./config/build/types/config";
 import path from "path";
 
-
-
 export default (env: BuildEnv) => {
     const paths: BuildPath = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
@@ -15,7 +13,6 @@ export default (env: BuildEnv) => {
     const mode = env.mode || 'development';
     const PORT = env.port || 3000;
     const isDev = mode === 'development';
-
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
