9256121694cd03334c4e6e1193e51d43ec8d93da
Samat Toichuev <samat25774@gmail.com>
2023-02-06 00:50:50 +0600

    created theme hook with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import './index.scss';
+import React, { Suspense, useContext, useState } from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+	const {theme, toggleTheme} = useTheme();
+
 	return (
-		<div className="app">
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>
+				Change Theme
+			</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
-			<Routes>
-				<Route path={'/about'} element={<AboutPage />}/>
-				<Route path={'/'} element={<MainPage />}/>
-			</Routes>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+						<Route path={'/about'} element={<AboutPageAsync />}/>
+						<Route path={'/'} element={<MainPageAsync />}/>
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
