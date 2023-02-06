016f9b72bb632ef798a4728f19f53bec5dcd0a01
Samat Toichuev <s.toychuyev@godigital.rocks>
2023-02-06 17:29:40 +0600

    first ui kolhoz component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,18 @@ import {Link } from "react-router-dom";
 import { useTheme } from 'app/providers/ThemeProvider';
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
 	const {theme, toggleTheme} = useTheme();
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
+			<Navbar />
+			<AppRouter />
 			<button onClick={toggleTheme}>
 				Change Theme
 			</button>
-			<Link to={'/'}>Главная</Link>
-			<Link to={'/about'}>О сайте</Link>
-			<AppRouter />
 		</div>
 	);
 };
