bd60c656901015e5659afb8c19e8ad6ae5b78f7e
Samat Toichuev <s.toychuyev@godigital.rocks>
2023-02-06 13:04:16 +0600

    added className helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { Suspense, useContext, useState } from 'react';
+import React, { Suspense } from 'react';
 import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { Theme, ThemeContext } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
 	const {theme, toggleTheme} = useTheme();
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<button onClick={toggleTheme}>
 				Change Theme
 			</button>
