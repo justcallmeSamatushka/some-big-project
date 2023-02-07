f454f1fb77e049ea5bdb141c883c4d122049da75
Samat Toichuev <s.toychuyev@godigital.rocks>
2023-02-07 13:10:12 +0600

    added sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -12,7 +12,11 @@ export const AppRouter = () => {
 					Object.values(routeConfig).map(({ path, element }) => (
 						<Route
 							key={path}
-							element={element}
+							element={(
+								<div className="page-wrapper">
+									{ element }
+								</div>
+							)}
 							path={path}
 						/>
 					))
