46dde8fd4284448f4bb3dd59e98b0eeb686c7070
Samat Toichuev <s.toychuyev@godigital.rocks>
2023-02-07 18:30:52 +0600

    added i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,9 +1,9 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
+import webpack, {DefinePlugin} from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
 	return [
 		new HtmlWebpackPlugin({
 			template: paths.html
@@ -12,6 +12,9 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
 		new MiniCssExtractPlugin({
 			filename: 'css/[name].[contenthash:8].css',
 			chunkFilename: 'css/[name].[contenthash:8].css'
+		}),
+		new DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev)
 		})
 	]
 }
