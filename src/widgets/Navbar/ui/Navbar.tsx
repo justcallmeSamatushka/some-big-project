99125330dc14809f43c4bf21a842b4e450536362
Samat Toichuev <s.toychuyev@godigital.rocks>
2023-02-09 14:05:23 +0600

    added stylelint, i18nlint plugin for eslint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
 	className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+	const { t } = useTranslation();
+
 	return (
 		<div className={classNames(cls.Navbar, {}, [className])}>
 			<div className={cls.links}>
@@ -16,10 +19,10 @@ export const Navbar = ({ className }: NavbarProps) => {
 					to={'/'}
 					className={cls.mainLink}
 				>
-					Главная
+					{t('main')}
 				</AppLink>
 				<AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-					О сайте
+					{t('about')}
 				</AppLink>
 			</div>
 		</div>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import cls from './Sidebar.module.scss';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {
 	className?: string;
@@ -10,6 +11,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
 	const [collapsed, setCollapsed] = useState(false);
+	const { t } = useTranslation();
 
 	const onToggle = () => {
 		setCollapsed((prev) => !prev);
@@ -21,7 +23,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 				className,
 			])}
 		>
-			<button onClick={onToggle}>toggle</button>
+			<button onClick={onToggle}>{t('toggle')}</button>
 			<div className={cls.switchers}>
 				<ThemeSwitcher />
 				<LangSwitcher className={cls.lang} />
