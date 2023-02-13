11088b3bc144fabc727d5092ffb67e8898b27c24
Samat Toichuev <s.toychuyev@godigital.rocks>
2023-02-13 14:06:01 +0600

    added ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+import React, { ErrorInfo, ReactNode, Suspense } from 'react';
+import { types } from 'sass';
+import Error = types.Error;
+import { ErrorWidget } from 'widgets/ErrorWidget';
+
+interface ErrorBoundaryProps {
+	children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+	hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+	ErrorBoundaryProps,
+	ErrorBoundaryState
+> {
+	constructor(props: ErrorBoundaryProps) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	static getDerivedStateFromError() {
+		// Update state so the next render will show the fallback UI.
+		return { hasError: true };
+	}
+
+	componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+		// You can also log the error to an error reporting service
+		console.log(error, errorInfo);
+	}
+
+	render() {
+		if (this.state.hasError) {
+			// You can render any custom fallback UI
+			return (
+				<Suspense fallback="">
+					<ErrorWidget />
+				</Suspense>
+			);
+		}
+
+		return this.props.children;
+	}
+}
